c694fbe6e82a2445d956424eb31de0077f247273
herrlado <herrlado@5a0c0748-9336-11de-a83d-5bdee9236848>
2009-09-01 16:15:57 +0000

    1.2.2

--- trunk/makesis.sh
+++ trunk/makesis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1.2.1"
+VERSION="1.2.2"
 # make distro
 rm -rf build
 mkdir -p build
@@ -17,9 +17,9 @@ cp -v versions.py build/
 cp -v utils.py build/
 cp -v res/manual.html build/
 TARGET=geoinput-$VERSION.sis
-MERGED_TARGET=geoinput-121.sis
+MERGED_TARGET=geoinput-122.sis
 # create sis-filex
-def#ensymble.py py2sis --uid=0xe3e34da2 --appname="geinput" --shortcaption="geoinput" --version=$VERSION --vendor="Lado Kumsiashvili" --verbose build geoinput-${VERSION}.sis
+#ensymble.py py2sis --uid=0xe3e34da2 --appname="geinput" --shortcaption="geoinput" --version=$VERSION --vendor="Lado Kumsiashvili" --verbose build geoinput-${VERSION}.sis
 ensymble.py py2sis --textfile=res/info.txt --vendor="Lado Kumsiashvili" --appname="geoinput" --uid=0x20027ad1 --caps=SwEvent --verbose --icon=res/geo_flag.svgt --version=$VERSION build sis/$TARGET
 ensymble.py mergesis sis/$TARGET libs/envy-1.0.4.sis libs/keypress-1.0.6.sis libs/appswitch-1.0.3.sis libs/applist-1.0.0.sis sis/$MERGED_TARGET
 rm -rf build
