6368b00de413e013efd229d841f8e26fcc53e9cd
herrlado <herrlado@5a0c0748-9336-11de-a83d-5bdee9236848>
2009-10-07 00:41:10 +0000

    help überarbetet. E71 als Basis Klasse für E63

--- trunk/versions.py
+++ trunk/versions.py
@@ -16,18 +16,21 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 
-#Nokia E61  RM-89, RM-227, RM-294
-#Nokia E71  RM-346, RM-357, RM-407, RM-493, RM-462
-#Nokia E63  RM-437, RM-449, RM-450
-#Nokia E72  RM-530, RM-529
-KBD_V = [u'RM-346',  u'RM-357', u'RM-407', u'RM-493', u'RM-462', u'RM-437', u'RM-449', u'RM-450', u'RM-530', u'RM-529']
 
+
+#Nokia E63  RM-437, RM-449, RM-450
+KBD_E63_V = [u'RM-437', u'RM-449', u'RM-450', u'RM-530', u'RM-529']
+
+#Nokia E61  RM-89, RM-227, RM-294
 KBD_E61_V = [u'RM-89', u'RM-227', u'RM-294']
 
-#Nokia E70      RM-10,	RM-24
-#Nokia E90      RA-6, RA-7
-DBL_KBD_V = [u'RM-10',u'RM-24',u'RA-6',u'RA-7']
+#Nokia E71  RM-346, RM-357, RM-407, RM-493, RM-462
+#Nokia E72  RM-530, RM-529
+KBD_E71_V = [u'RM-346', u'RM-357', u'RM-407', u'RM-493', u'RM-462',u'RM-530', u'RM-529']
 
+#Nokia E70      RM-10, RM-24
+#Nokia E90      RA-6,  RA-7
+DBL_KBD_V = [u'RM-10',u'RM-24',u'RA-6',u'RA-7']
 
 
 #Nokia N97
